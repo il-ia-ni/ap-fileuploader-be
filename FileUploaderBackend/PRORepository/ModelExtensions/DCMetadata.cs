e3744c16d741d73254e78c25d5d2f54a5fbaf5a6
Ilia Nikolaenko <aili70@duck.com>
2023-10-26 17:30:25 +0200

    Created a library pro for data first EF Core, conf projects

---
+++ FileUploaderBackend/PRORepository/ModelExtensions/DCMetadata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PRORepository
+{
+    public partial class DCMetadata
+    {
+        [Timestamp]
+        public byte[]? RowVersion { get; set; }  // this prop is used for tracking lazy concurrency
+
+        public override string ToString()
+        {
+            return "This is a test value of DCMetadata class";
+        }
+    }
+}
