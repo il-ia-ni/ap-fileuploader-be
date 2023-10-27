59f8da3a6410a43eff91a7dc67e0ddb211076755
Ilia Nikolaenko <niil02@sms-group.com>
2023-10-27 11:48:34 +0200

    Web API: Added PROMetadata controller for accessing DBContext from external env

---
+++ FileUploaderBackend/FileUploaderBackend/Controllers/ProMetadataController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProLibrary.Models;
+using FileUploaderBackend.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FileUploaderBackend.Controllers
+{
+    public class ProMetadataController : Controller
+    {
+        
+    }
+}
