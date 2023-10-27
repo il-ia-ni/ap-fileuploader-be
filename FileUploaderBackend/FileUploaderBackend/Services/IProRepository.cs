a8fca81c0267f950db67b3005768f930dc3e8099
Ilia Nikolaenko <niil02@sms-group.com>
2023-10-27 11:44:20 +0200

    Web API: Upd ProRepository logic of CRUD-Methods, got rid of pessim concurrency

--- FileUploaderBackend/FileUploaderBackend/Services/IProRepository.cs
+++ FileUploaderBackend/FileUploaderBackend/Services/IProRepository.cs
@@ -5,12 +5,11 @@ namespace FileUploaderBackend.Services
     public interface IProRepository
     {
         Task CreateMetadataItem(DcMetadata mdEntity);
-        void DeleteAllMdItems();
-        void DeleteSingleMetadataItem(int mdId);
+        Task DeleteAllMdItems();
+        Task DeleteSingleMetadataItem(int mdId);
         Task<ICollection<DcMetadata>> GetAllMetadataItems();
         Task<DcMetadata?> GetSingleMetadataItem(int mdId);
-        void Save();
-        Task<int> SaveWithPessimisticConcurrency();
-        void UpdateMetadataItem(int mdId, DcMetadata mdEntity);
+        Task Save();
+        Task UpdateMetadataItem(int mdId, DcMetadata mdEntity);
     }
 }
--- FileUploaderBackend/FileUploaderBackend/Services/ProRepository.cs
+++ FileUploaderBackend/FileUploaderBackend/Services/ProRepository.cs
@@ -1,21 +1,27 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
-using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
 using ProLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+using System.Threading.Tasks;
 
 namespace FileUploaderBackend.Services
 {
     public class ProRepository : IProRepository
     {
         private readonly PROContext _dbContext;
+        private readonly ILogger<ProRepository> _logger;
 
-        public ProRepository(PROContext dbContext)
+        public ProRepository(PROContext dbContext, ILogger<ProRepository> logger)
         {
             _dbContext = dbContext;
+            _logger = logger;
         }
 
-        public virtual async Task<ICollection<DcMetadata>> GetAllMetadataItems()
+        public async Task<ICollection<DcMetadata>> GetAllMetadataItems()
         {
             try
             {
@@ -23,24 +29,25 @@ namespace FileUploaderBackend.Services
             }
             catch (Exception ex)
             {
-                throw new Exception($"Couldn't retrieve metadata entities: {ex.Message}");
+                _logger.LogError(ex, "Error while retrieving metadata entities");
+                throw;
             }
         }
 
-        public virtual async Task<DcMetadata?> GetSingleMetadataItem(int mdId)
+        public async Task<DcMetadata?> GetSingleMetadataItem(int mdId)
         {
-
             try
             {
                 return await _dbContext.DcMetadata.FindAsync(mdId);
             }
             catch (Exception ex)
             {
-                throw new Exception($"Couldn't retrieve the specified metadata entity: {ex.Message}");
+                _logger.LogError(ex, "Error while retrieving the specified metadata entity");
+                throw;
             }
         }
 
-        public virtual async Task CreateMetadataItem(DcMetadata mdEntity)
+        public async Task CreateMetadataItem(DcMetadata mdEntity)
         {
             if (mdEntity == null)
             {
@@ -48,114 +55,82 @@ namespace FileUploaderBackend.Services
             }
             try
             {
-                if (mdEntity != null)
-                {
-                    await _dbContext.DcMetadata.AddAsync(mdEntity);
-                }
+                await _dbContext.DcMetadata.AddAsync(mdEntity);
+                await Save();
             }
             catch (Exception ex)
             {
-                throw new Exception($"{nameof(mdEntity)} could not be added: {ex.Message}");
+                _logger.LogError(ex, $"{nameof(mdEntity)} could not be added");
+                throw;
             }
         }
 
-        public async void UpdateMetadataItem(int mdId, DcMetadata mdEntity)
+        public async Task UpdateMetadataItem(int mdId, DcMetadata mdEntity)
         {
-            DcMetadata mdItemStored = await GetSingleMetadataItem(mdId);
-
-            mdItemStored.ItemSource = String.IsNullOrEmpty(mdEntity.ItemSource) ? mdItemStored.ItemSource : mdEntity.ItemSource;
-            mdItemStored.Host = String.IsNullOrEmpty(mdEntity.Host) ? mdItemStored.Host : mdEntity.Host;
-            mdItemStored.ItemContainer = String.IsNullOrEmpty(mdEntity.ItemContainer) ? mdItemStored.ItemContainer : mdEntity.ItemContainer;
-            mdItemStored.ItemName = String.IsNullOrEmpty(mdEntity.ItemName) ? mdItemStored.ItemName : mdEntity.ItemName;
-            mdItemStored.ItemType = String.IsNullOrEmpty(mdEntity.ItemType) ? mdItemStored.ItemType : mdEntity.ItemType;
-            mdItemStored.ItemComment = String.IsNullOrEmpty(mdEntity.ItemComment) ? mdItemStored.ItemComment : mdEntity.ItemComment;
-            mdItemStored.Unit = String.IsNullOrEmpty(mdEntity.Unit) ? mdItemStored.Unit : mdEntity.Unit;
-
-
-            mdItemStored.Scaling = mdEntity.Scaling is null ? mdItemStored.Scaling : mdEntity.Scaling;
-            mdItemStored.UpdateCycle = mdEntity.UpdateCycle is null ? mdItemStored.UpdateCycle : mdEntity.UpdateCycle;
-
-            mdItemStored.Sensor = String.IsNullOrEmpty(mdEntity.Sensor) ? mdItemStored.Sensor : mdEntity.Sensor;
-
-            mdItemStored.MinVal = mdEntity.MinVal is null ? mdItemStored.MinVal : mdEntity.MinVal;
-            mdItemStored.MaxVal = mdEntity.MaxVal is null ? mdItemStored.MaxVal : mdEntity.MaxVal;
-
-            mdItemStored.Orientation = String.IsNullOrEmpty(mdEntity.Orientation) ? mdItemStored.Orientation : mdEntity.Orientation;
-
-
-            await SaveWithPessimisticConcurrency();
-        }
-
-        public void DeleteAllMdItems()
-        {
-            _dbContext.Database.ExecuteSqlRawAsync("TRUNCATE TABLE [DC_METADATA]");
-            // For raw sql in EF core 6 see https://stackoverflow.com/a/18986676
-            Save();
-        }
-
-        public void DeleteSingleMetadataItem(int mdId)
-        {
-            _dbContext.DcMetadata.Remove(GetSingleMetadataItem(mdId).Result);
-            SaveWithPessimisticConcurrency();
-        }
-
-        public virtual void Save()
-        {
-            _dbContext.SaveChanges();
-        }
-
-        public virtual async Task<int> SaveWithPessimisticConcurrency()
-        {
-            int records = 0;
-            IDbContextTransaction tx = null;
-
             try
             {
-                using (tx = await _dbContext.Database.BeginTransactionAsync())
-                {
-                    records = await _dbContext.SaveChangesAsync();
-                    tx.Commit();
-                    return records;
-                }
+                DcMetadata mdItemStored = await GetSingleMetadataItem(mdId);
+
+                mdItemStored.ItemSource = String.IsNullOrEmpty(mdEntity.ItemSource) ? mdItemStored.ItemSource : mdEntity.ItemSource;
+                mdItemStored.Host = String.IsNullOrEmpty(mdEntity.Host) ? mdItemStored.Host : mdEntity.Host;
+                mdItemStored.ItemContainer = String.IsNullOrEmpty(mdEntity.ItemContainer) ? mdItemStored.ItemContainer : mdEntity.ItemContainer;
+                mdItemStored.ItemName = String.IsNullOrEmpty(mdEntity.ItemName) ? mdItemStored.ItemName : mdEntity.ItemName;
+                mdItemStored.ItemType = String.IsNullOrEmpty(mdEntity.ItemType) ? mdItemStored.ItemType : mdEntity.ItemType;
+                mdItemStored.ItemComment = String.IsNullOrEmpty(mdEntity.ItemComment) ? mdItemStored.ItemComment : mdEntity.ItemComment;
+                mdItemStored.Unit = String.IsNullOrEmpty(mdEntity.Unit) ? mdItemStored.Unit : mdEntity.Unit;
+
+
+                mdItemStored.Scaling = mdEntity.Scaling is null ? mdItemStored.Scaling : mdEntity.Scaling;
+                mdItemStored.UpdateCycle = mdEntity.UpdateCycle is null ? mdItemStored.UpdateCycle : mdEntity.UpdateCycle;
+
+                mdItemStored.Sensor = String.IsNullOrEmpty(mdEntity.Sensor) ? mdItemStored.Sensor : mdEntity.Sensor;
+
+                mdItemStored.MinVal = mdEntity.MinVal is null ? mdItemStored.MinVal : mdEntity.MinVal;
+                mdItemStored.MaxVal = mdEntity.MaxVal is null ? mdItemStored.MaxVal : mdEntity.MaxVal;
+
+                mdItemStored.Orientation = String.IsNullOrEmpty(mdEntity.Orientation) ? mdItemStored.Orientation : mdEntity.Orientation;
+
+
+                await Save();
             }
-            catch (DbUpdateConcurrencyException ex)  // area for resolving a concurrency conflict (thrown only on update and delete, never on add): https://learn.microsoft.com/en-us/ef/core/saving/concurrency?tabs=data-annotations#resolving-concurrency-conflicts
+            catch (Exception ex)
             {
-                foreach (var entry in ex.Entries)
-                {
-                    if (entry.Entity is DcMetadata)
-                    {
-                        var originalValues = entry.OriginalValues;  // originally retrieved before any edits 
-                        var proposedValues = entry.CurrentValues;  // application was attempting to write
-                        var databaseValues = entry.GetDatabaseValues();  // currently stored in the database
+                _logger.LogError(ex, "Error while updating metadata item");
+                throw;
+            }
+        }
 
-                        foreach (var property in proposedValues.Properties)
-                        {
-                            var proposedValue = proposedValues[property];
-                            var databaseValue = databaseValues[property];
+        public async Task DeleteAllMdItems()
+        {
+            await _dbContext.Database.ExecuteSqlRawAsync("TRUNCATE TABLE [DC_METADATA]");
+            // For raw sql in EF core 6 see https://stackoverflow.com/a/18986676
+            await Save();
+        }
 
-                            // TODO: decide which value should be written to database
-                            proposedValues[property] = proposedValue;
-                        }
+        public async Task DeleteSingleMetadataItem(int mdId)
+        {
+            DcMetadata mdItem = await GetSingleMetadataItem(mdId);
+            _dbContext.DcMetadata.Remove(mdItem);
+            await Save();
+        }
 
-                        // Refresh original values to bypass next concurrency check
-                        entry.OriginalValues.SetValues(databaseValues);
-
-                        await SaveWithPessimisticConcurrency();
-                    }
-                    else
-                    {
-                        throw new NotSupportedException("Unable to save changes. The metadata item details were updated by another user. " + entry.Metadata.Name);
-                    }
-                }
-                throw ex;
+        public async Task Save()
+        {
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                _logger.LogError(ex, "Concurrency conflict");
+                // Hier die Konfliktauflösung implementieren (falls gebraucht)
+                throw;
             }
             catch (DbUpdateException ex)
             {
-                SqlException s = ex.InnerException as SqlException;
-                var errorMessage = $"{ex.Message}" + " {ex?.InnerException.Message}" + " rolling back…";
-                tx.Rollback();
+                _logger.LogError(ex, "Database update error");
+                throw;
             }
-            return records;
         }
     }
 }
