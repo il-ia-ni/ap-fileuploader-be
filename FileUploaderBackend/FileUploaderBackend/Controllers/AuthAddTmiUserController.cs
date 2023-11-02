a9fd00ec5c37d60eab68d9ecaf7c680590916f90
Ilia Nikolaenko <aili70@duck.com>
2023-11-02 17:32:03 +0100

    FileUploaderBE - added Auth Controllers

---
+++ FileUploaderBackend/FileUploaderBackend/Controllers/AuthAddTmiUserController.cs
@@ -0,0 +1,45 @@
+﻿using FileUploaderBackend.DTOs;
+using FileUploaderBackend.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProLibrary.Models;
+
+namespace FileUploaderBackend.Controllers
+{
+    //[Authorize(Roles = "admin")]
+    [Route("api/auth/adduser")]
+    [ApiController]
+    public class AuthAddTmiUserController : Controller
+    {
+        private readonly IAuthServicePSK _authService;
+        public AuthAddTmiUserController(IAuthServicePSK authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        public IActionResult AddUser([FromBody] UserLoginDto newUser, [FromBody] string password, [FromBody] string role = "")
+        {
+            if (string.IsNullOrEmpty(newUser.Username) || string.IsNullOrEmpty(password))
+            {
+                return BadRequest("Username and password are required.");
+            }
+
+            if (_authService.UserExists(newUser.Username))
+            {
+                return BadRequest("User with the same username already exists.");
+            }
+
+            bool isUserCreated = _authService.CreateUser(newUser, password, role);
+
+            if (isUserCreated)
+            {
+                return Ok("User registration successful.");
+            }
+            else
+            {
+                return StatusCode(500, "Failed to create user. Please try again.");
+            }
+        }
+    }
+}
---
+++ FileUploaderBackend/FileUploaderBackend/Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using FileUploaderBackend.Services;
+using FileUploaderBackend.DTOs;
+
+namespace FileUploaderBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IAuthServicePSK _authService;
+
+        public AuthController(IConfiguration configuration, IAuthServicePSK authService)
+        {
+            _configuration = configuration;
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] UserLoginDto loginRequestData)
+        {
+            var username = loginRequestData.Username;
+            var password = loginRequestData.Password;
+
+            if (_authService.AuthenticateUser(username, password))
+            {
+                // Authentifizierung erfolgreich
+                // Hier können Sie ein JWT-Token erstellen und zurückgeben
+                var token = GenerateJwtToken(username);
+
+                return Ok(new { token });
+            }
+
+            // Authentifizierung fehlgeschlagen
+            return Unauthorized("Invalid username or password");
+        }
+
+        private string GenerateJwtToken(string username)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(
+                _configuration.GetSection("TokenKeys:DEV").Value);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, username)
+                }),
+                Expires = DateTime.UtcNow.AddHours(3), // Expiry time of the Token
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+
+
+}
